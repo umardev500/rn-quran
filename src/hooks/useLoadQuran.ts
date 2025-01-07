debfb44a1a4b94b4e4ae21ce9ce0a8691a56137c
Umar Schweinsteiger <umar.dev500@gmail.com>
2025-01-07 11:24:41 +0700

    fix quran not loaded because we not copy the data

--- src/hooks/useLoadQuran.ts
+++ src/hooks/useLoadQuran.ts
@@ -1,6 +1,6 @@
 import {Quran} from '@typed';
 import {useCallback, useEffect, useState} from 'react';
-import {fetchQuranData} from '@utils';
+import {copyQuranAssetsToDocuments, fetchQuranData} from '@utils';
 
 export const useLoadQuran = (sura: number) => {
   const [loading, setLoading] = useState(true);
@@ -12,6 +12,7 @@ export const useLoadQuran = (sura: number) => {
     setErr(null); // Reset error before starting
 
     try {
+      await copyQuranAssetsToDocuments();
       const quranData = await fetchQuranData(sura);
       setData(quranData);
     } catch (error) {
