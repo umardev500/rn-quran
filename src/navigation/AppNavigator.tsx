40d010710e5f3a4e25986595c855f13ace7b9318
Umar Schweinsteiger <umar.dev500@gmail.com>
2025-01-08 13:07:42 +0700

    use transitino presets ios

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,7 +1,7 @@
-import {createStackNavigator} from '@react-navigation/stack';
-import {HomeTabNavigator} from './HomeTabNavigator';
+import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
 import {ReadScreen} from '@screens';
 import {RootStackParamList} from '@typed';
+import {HomeTabNavigator} from './HomeTabNavigator';
 
 const Stack = createStackNavigator<RootStackParamList>();
 
@@ -9,7 +9,10 @@ export const AppNavigator = () => {
   return (
     <Stack.Navigator
       initialRouteName="HomeTabs"
-      screenOptions={{headerShown: false}}>
+      screenOptions={{
+        headerShown: false,
+        ...TransitionPresets.SlideFromRightIOS,
+      }}>
       <Stack.Screen name="HomeTabs" component={HomeTabNavigator} />
       <Stack.Screen name="Read" component={ReadScreen} />
     </Stack.Navigator>
